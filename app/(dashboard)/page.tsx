3406cd56698fa3ca0995c3067c2730232957436c
lzqailq <1493495317@qq.com>
2024-08-04 16:10:41 +0800

    feat:添加content empty情况

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,17 @@
 "use client"
+import { useOrganization } from "@clerk/nextjs"
+
+import { EmptyOrg } from "./_components/empty-org"
 
 const DashboardPage = () => {
+    const {organization} = useOrganization()
     return (
-        <div>
-            Dashboard Root pagefdsafds
-            
+        <div className="flex-1 h-[calc(100%-80px)] p-6">
+            {!organization ? (
+                <EmptyOrg />) : <p>
+                    Board List</p>
+            }
+           
         </div>
     )
 }
