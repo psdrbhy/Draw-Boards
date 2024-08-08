a4545dca753c49e11052c041c206bd4eb7abac1c
lzqailq <1493495317@qq.com>
2024-08-08 17:20:49 +0800

    feat:开发partcipant组件以及初始化画板工具箱组件

--- app/board/[boardId]/_components/participant.tsx
+++ app/board/[boardId]/_components/participant.tsx
@@ -1,16 +1,52 @@
+"use client";
 
-// 画布右边参与人组件
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { UserAvatar } from "./user-avatar";
+import { connectionIdToColor } from "@/lib/utils";
+
+const MAX_SHOW_USERS = 2;
+
 export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOW_USERS;
+
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      List of users
+      <div className="flex gap-x-2">
+        {/* board正在参与的用户 */}
+        {users.slice(0, MAX_SHOW_USERS).map(({ connectionId, info }) => (
+          <UserAvatar
+            borderColor={connectionIdToColor(connectionId)}
+            key={connectionId}
+            src={info?.picture}
+            name={info?.name}
+            fallback={info?.name?.[0] || "T"}
+          />
+        ))}
+        {/* 自己 */}
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (You)`}
+            fallback={currentUser.info?.name?.[0]}
+          />
+        )}
+        {/* 超过的 */}
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOW_USERS} more`}
+            fallback={`+${users.length - MAX_SHOW_USERS}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
 
 export const ParticipantsSkeleton = function () {
-    return (
-      <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]"></div>
-    );
-  };
-
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]"></div>
+  );
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+
+const COLORS = ['#DC2626', '#D97706', '#059669', '#7C3AED', '#DB2777']
 // 解决一些css的合并重复和冲突
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// 随机color
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length]
 }
