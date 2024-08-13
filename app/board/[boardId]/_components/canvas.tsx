05e00f6d1f7658ad7f76975f5d86cff023fe1573
lzqailq <1493495317@qq.com>
2024-08-13 21:43:55 +0800

    feat:添加删除快捷键

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -431,6 +431,9 @@ export const Canvas = ({ boardId }: CanvasProps) => {
   useEffect(() => {
     function onKeyDown(e: KeyboardEvent) {
       switch (e.key) {
+        case "Backspace":
+          deleteLayers();
+          break;
         case "z": {
           if (e.ctrlKey || e.metaKey) {
             if (e.shiftKey) {
